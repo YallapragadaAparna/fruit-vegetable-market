fd6d407f192b2ab528efc55d67bbdaa342ccb5c2
YallapragadaAparna <yaparna2005@gmail.com>
2026-05-01 19:31:06 +0530

    Enhance error handling in AdminProducts component
    Refactor AdminProducts component to handle errors and improve code structure.

--- frontend/myapp/src/components/AdminProducts/AdminProducts.js
+++ frontend/myapp/src/components/AdminProducts/AdminProducts.js
@@ -1,7 +1,223 @@
-import React, { useState, useEffect,useRef } from "react";
+// import React, { useState, useEffect,useRef } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// import api,{IMAGE_URL} from "../../services/api";
+// import "./AdminProducts.css";
+
+// function AdminProducts() {
+
+//   const navigate = useNavigate();
+//   const fileInputRef = useRef(null);
+//   const [products, setProducts] = useState([]);
+
+//   const [name, setName] = useState("");
+//   const [price, setPrice] = useState("");
+//   const [category, setCategory] = useState("");
+//   const [image, setImage] = useState(null);
+//   const [stock, setStock] = useState("");
+
+//   const [editId, setEditId] = useState(null);
+//   //const [preview, setPreview] = useState(null);
+
+//   // LOGOUT
+//   const handleLogout = () => {
+//     localStorage.removeItem("token");
+//     navigate("/");
+//   };
+
+//   // GET PRODUCTS
+//   const fetchProducts = async () => {
+//     const res = await api.get("/products");
+//     setProducts(res.data);
+//   };
+
+//   useEffect(() => {
+//     fetchProducts();
+//   }, []);
+
+//   // ADD / UPDATE PRODUCT
+//   const addProduct = async (e) => {
+//     e.preventDefault();
+
+//     const formData = new FormData();
+
+//     formData.append("name", name);
+//     formData.append("price", price);
+//     formData.append("category", category);
+//     formData.append("stock", stock);
+
+//     if (image) {
+//       formData.append("image", image);
+//     }
+
+//     if (editId) {
+//       await api.put(`/products/${editId}`, formData);
+//     } else {
+//       await api.post("/products", formData);
+//     }
+
+//     fetchProducts();
+
+//     setName("");
+//     setPrice("");
+//     setCategory("");
+//     setStock("");
+//     setImage(null);
+//     //setPreview(null);
+//     setEditId(null);
+//     if (fileInputRef.current) {
+//   fileInputRef.current.value = "";
+// }
+
+//   };
+
+//   // DELETE PRODUCT
+//   const deleteProduct = async (id) => {
+//     await api.delete(`/products/${id}`);
+//     fetchProducts();
+//   };
+
+//   return (
+
+//     <div className="admin-products">
+
+//       {/* HEADER */}
+//       <div className="admin-header">
+//         <h2>Admin Product Management</h2>
+
+//          <div className="header-buttons">
+//         <button onClick={() => navigate("/admin-orders")}className="view-orders-btn">
+// View Orders
+// </button>
+// <button onClick={handleLogout} className="logout-btn">
+//           Logout
+//         </button>
+//       </div>
+//       </div>
+
+//       {/* PRODUCT FORM */}
+//       <form onSubmit={addProduct} className="product-form">
+
+//         <input
+//           type="text"
+//           placeholder="Product Name"
+//           value={name}
+//           onChange={(e) => setName(e.target.value)}
+//           required
+//         />
+
+//         <input
+//           type="number"
+//           placeholder="Price"
+//           value={price}
+//           onChange={(e) => setPrice(e.target.value)}
+//           required
+//         />
+
+//         {/* CATEGORY */}
+//         <select
+//           value={category}
+//           onChange={(e) => setCategory(e.target.value)}
+//           required
+//         >
+//           <option value="">Select Category</option>
+//           <option value="Fruit">Fruit</option>
+//           <option value="Vegetable">Vegetable</option>
+//         </select>
+
+//                 {/* IMAGE UPLOAD */}
+//         <input
+//           type="file"
+//            ref={fileInputRef}
+//           onChange={(e) => setImage(e.target.files[0])}
+//          required
+//        />
+
+//         {/* STOCK */}
+//         <select
+//           value={stock}
+//           onChange={(e) => setStock(e.target.value)}
+//           required
+//         >
+//           <option value="">Select Stock Status</option>
+//           <option value="Full">Full</option>
+//           <option value="Limited">Limited</option>
+//           <option value="Out of Stock">Out of Stock</option>
+//         </select>
+
+//         <button type="submit">
+//           {editId ? "Update Product" : "Add Product"}
+//         </button>
+
+//       </form>
+
+//       {/* PRODUCT LIST */}
+//       <div className="product-list">
+
+//         {products.map((product) => (
+
+//           <div key={product._id} className="product-card">
+
+//             <img
+//               src={`${IMAGE_URL}${product.image}`}
+//               alt={product.name}
+//             />
+
+
+//             <h3>{product.name}</h3>
+
+//             <p className="price">Price: ₹{product.price}</p>
+
+//             {/* CATEGORY BADGE */}
+//             <p className={`category ${product.category.toLowerCase()}`}>
+//               {product.category}
+//             </p>
+
+//             {/* STOCK BADGE */}
+//             <p className={`stock-status ${product.stock.replace(/\s/g,"").toLowerCase()}`}>
+//               {product.stock}
+//             </p>
+
+//             <div className="card-buttons">
+
+//               <button
+//                 onClick={() => deleteProduct(product._id)}
+//                 className="delete-btn"
+//               >
+//                 Delete
+//               </button>
+
+//               <button
+//                 className="update-btn"
+//                 onClick={() => {
+//                   setEditId(product._id);
+//                   setName(product.name);
+//                   setPrice(product.price);
+//                   setCategory(product.category);
+//                   setStock(product.stock);
+//                 }}
+//               >
+//                 Update
+//               </button>
+              
+
+//             </div>
+
+//           </div>
+
+//         ))}
+
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// export default AdminProducts;
+import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
-import api,{IMAGE_URL} from "../../services/api";
+import api from "../../services/api"; // ✅ removed IMAGE_URL
 import "./AdminProducts.css";
 
 function AdminProducts() {
@@ -17,7 +233,6 @@ function AdminProducts() {
   const [stock, setStock] = useState("");
 
   const [editId, setEditId] = useState(null);
-  //const [preview, setPreview] = useState(null);
 
   // LOGOUT
   const handleLogout = () => {
@@ -27,8 +242,12 @@ function AdminProducts() {
 
   // GET PRODUCTS
   const fetchProducts = async () => {
-    const res = await api.get("/products");
-    setProducts(res.data);
+    try {
+      const res = await api.get("/products");
+      setProducts(res.data);
+    } catch (error) {
+      console.log("FETCH ERROR:", error);
+    }
   };
 
   useEffect(() => {
@@ -50,49 +269,61 @@ function AdminProducts() {
       formData.append("image", image);
     }
 
-    if (editId) {
-      await api.put(`/products/${editId}`, formData);
-    } else {
-      await api.post("/products", formData);
+    try {
+      if (editId) {
+        await api.put(`/products/${editId}`, formData);
+      } else {
+        await api.post("/products", formData);
+      }
+
+      fetchProducts();
+
+      // reset form
+      setName("");
+      setPrice("");
+      setCategory("");
+      setStock("");
+      setImage(null);
+      setEditId(null);
+
+      if (fileInputRef.current) {
+        fileInputRef.current.value = "";
+      }
+
+    } catch (error) {
+      console.log("ADD/UPDATE ERROR:", error);
     }
-
-    fetchProducts();
-
-    setName("");
-    setPrice("");
-    setCategory("");
-    setStock("");
-    setImage(null);
-    //setPreview(null);
-    setEditId(null);
-    if (fileInputRef.current) {
-  fileInputRef.current.value = "";
-}
-
   };
 
   // DELETE PRODUCT
   const deleteProduct = async (id) => {
-    await api.delete(`/products/${id}`);
-    fetchProducts();
+    try {
+      await api.delete(`/products/${id}`);
+      fetchProducts();
+    } catch (error) {
+      console.log("DELETE ERROR:", error);
+    }
   };
 
   return (
-
     <div className="admin-products">
 
       {/* HEADER */}
       <div className="admin-header">
         <h2>Admin Product Management</h2>
 
-         <div className="header-buttons">
-        <button onClick={() => navigate("/admin-orders")}className="view-orders-btn">
-View Orders
-</button>
-<button onClick={handleLogout} className="logout-btn">
-          Logout
-        </button>
-      </div>
+        <div className="header-buttons">
+          <button
+            onClick={() => navigate("/admin-orders")}
+            className="view-orders-btn"
+          >
+            View Orders
+          </button>
+
+          <button onClick={handleLogout} className="logout-btn">
+            Logout
+          </button>
+        </div>
       </div>
 
       {/* PRODUCT FORM */}
@@ -125,13 +356,13 @@ View Orders
           <option value="Vegetable">Vegetable</option>
         </select>
 
-                {/* IMAGE UPLOAD */}
+        {/* IMAGE */}
         <input
           type="file"
-           ref={fileInputRef}
+          ref={fileInputRef}
           onChange={(e) => setImage(e.target.files[0])}
-         required
-       />
+          required={!editId} // ✅ FIXED
+        />
 
         {/* STOCK */}
         <select
@@ -155,26 +386,23 @@ View Orders
       <div className="product-list">
 
         {products.map((product) => (
-
           <div key={product._id} className="product-card">
 
+            {/* ✅ FIXED IMAGE */}
             <img
-              src={`${IMAGE_URL}${product.image}`}
+              src={product.image}
               alt={product.name}
             />
-
 
             <h3>{product.name}</h3>
 
             <p className="price">Price: ₹{product.price}</p>
 
-            {/* CATEGORY BADGE */}
             <p className={`category ${product.category.toLowerCase()}`}>
               {product.category}
             </p>
 
-            {/* STOCK BADGE */}
-            <p className={`stock-status ${product.stock.replace(/\s/g,"").toLowerCase()}`}>
+            <p className={`stock-status ${product.stock.replace(/\s/g, "").toLowerCase()}`}>
               {product.stock}
             </p>
 
@@ -199,16 +427,12 @@ View Orders
               >
                 Update
               </button>
-              
 
             </div>
-
           </div>
-
         ))}
 
       </div>
-
     </div>
   );
 }
