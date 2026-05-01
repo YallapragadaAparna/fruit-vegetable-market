20291257be0b5c5fc7bbc031df0bf4b5c2cb2f78
YallapragadaAparna <yaparna2005@gmail.com>
2026-05-01 20:40:53 +0530

    Update profileRoutes.js

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,30 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-
-// const authMiddleware = require("../middleware/authMiddleware");
-// const fileupload = require("../middleware/fileUpload");
-
-// const {
-//   getProfile,
-//   updateProfile,
-//   deletePhoto
-// } = require("../controllers/profileController");
-
-// // ✅ GET PROFILE
-// router.get("/", authMiddleware, getProfile);
-
-// // ✅ UPDATE PROFILE
-// router.post(
-//   "/update",
-//   authMiddleware,
-//   fileupload.single("image"),
-//   updateProfile
-// );
-
-// // ✅ DELETE PHOTO
-// router.delete("/photo", authMiddleware, deletePhoto);
-
-// module.exports = router;
 const express = require("express");
 const router = express.Router();
 
