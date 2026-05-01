3c42d6ff3f0b5933f5db4ed0ee2972f338892d88
YallapragadaAparna <yaparna2005@gmail.com>
2026-05-01 11:25:20 +0530

    Update AdminProducts.js

--- frontend/myapp/src/components/AdminProducts/AdminProducts.js
+++ frontend/myapp/src/components/AdminProducts/AdminProducts.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect,useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
-import api, { IMAGE_URL } from "../../services/api";
+import api from "../../services/api";
 import "./AdminProducts.css";
 
 function AdminProducts() {
