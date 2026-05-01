065bbcc0f6d50d1f16b3c3be172240a122f301cb
YallapragadaAparna <yaparna2005@gmail.com>
2026-05-01 10:50:40 +0530

    Refactor file upload to use Cloudinary storage

--- backend/middleware/fileUpload.js
+++ backend/middleware/fileUpload.js
@@ -1,27 +1,45 @@
-// const multer = require("multer");
+// // const multer = require("multer");
 
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "uploads/");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + "-" + file.originalname);
+// // const storage = multer.diskStorage({
+// //   destination: (req, file, cb) => {
+// //     cb(null, "uploads/");
+// //   },
+// //   filename: (req, file, cb) => {
+// //     cb(null, Date.now() + "-" + file.originalname);
+// //   }
+// // });
+
+// // const fileUpload = multer({ storage });
+
+// // module.exports = fileUpload;
+// const multer = require("multer");
+// const { CloudinaryStorage } = require("multer-storage-cloudinary");
+// const cloudinary = require("../config/cloudinary");
+
+// const storage = new CloudinaryStorage({
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: "freshcart_uploads",
+//     allowed_formats: ["jpg", "png", "jpeg"]
 //   }
 // });
 
-// const fileUpload = multer({ storage });
+// const upload = multer({ storage });
 
-// module.exports = fileUpload;
+// module.exports = upload;
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../config/cloudinary");
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
-  params: {
-    folder: "freshcart_uploads",
-    allowed_formats: ["jpg", "png", "jpeg"]
-  }
+  params: async (req, file) => {
+    return {
+      folder: "freshcart_uploads",
+      allowed_formats: ["jpg", "png", "jpeg"],
+      public_id: Date.now() + "-" + file.originalname,
+    };
+  },
 });
 
 const upload = multer({ storage });
